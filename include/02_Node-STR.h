f040f661af5f0e388f9bee5e51c7dd8defda4e35
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-26 17:20:54 -0600

    feat: add pointer to the end of the nodes in struct graph and add int occurrences to the struct node

--- include/02_Node-STR.h
+++ include/02_Node-STR.h
@@ -12,10 +12,12 @@
 struct graph {
     int n_elements;
     struct node *start;
+    struct node *end;
 };
 
 struct node {
     char *word;
+    int occurrences;
     float probability;
     struct node *next;
     struct edge *connection;
@@ -34,6 +36,8 @@ struct auxiliar_words {
 };
 
 struct auxiliar_words *auxiliar_str(void);
-
+int security_check(struct node *node, char *word);
+void node_to_graph(struct graph *graph, struct auxiliar_words *auxiliar);
+void update_occurrences(struct node *node);
 
 #endif //LOW_LEVEL_NLP_02_NODE_STR_H
