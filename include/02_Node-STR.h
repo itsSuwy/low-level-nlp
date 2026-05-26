eae60beb64f61585c478ddc1f76fa1c1679b1ced
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-26 17:28:43 -0600

    feat: add insert element

--- include/02_Node-STR.h
+++ include/02_Node-STR.h
@@ -39,5 +39,6 @@ struct auxiliar_words *auxiliar_str(void);
 int security_check(struct node *node, char *word);
 void node_to_graph(struct graph *graph, struct auxiliar_words *auxiliar);
 void update_occurrences(struct node *node);
+void insert_element(struct graph *graph, struct node *node);
 
 #endif //LOW_LEVEL_NLP_02_NODE_STR_H
