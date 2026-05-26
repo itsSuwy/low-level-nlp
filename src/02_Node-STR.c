73b09f516e3bf99c5eb660abe154901fe7752aac
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-26 12:27:18 -0600

    feat: implement auxiliar_str to allocate memory for "struct auxiliar_words" using calloc

--- src/02_Node-STR.c
+++ src/02_Node-STR.c
@@ -3,3 +3,14 @@
 //
 
 #include "../include/02_Node-STR.h"
+#include "../include/01_Reading.h"
+
+#include <stdlib.h>
+
+struct auxiliar_words *auxiliar_str(void) {
+    struct auxiliar_words *auxiliar = (struct auxiliar_words*) calloc(1,sizeof(struct auxiliar_words));
+    if (!auxiliar) {
+        exit(-1);
+    }
+    return auxiliar;
+}
