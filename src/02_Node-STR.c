7ba23d261fa90a34c541e70c949494550d13b399
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-26 13:06:44 -0600

    feat: add initialize_pointers with safe allocation and string copy

--- src/02_Node-STR.c
+++ src/02_Node-STR.c
@@ -5,7 +5,7 @@
 #include "../include/02_Node-STR.h"
 #include "../include/01_Reading.h"
 
-#include <stdlib.h>
+
 
 struct auxiliar_words *auxiliar_str(void) {
     struct auxiliar_words *auxiliar = (struct auxiliar_words*) calloc(1,sizeof(struct auxiliar_words));
@@ -13,4 +13,23 @@ struct auxiliar_words *auxiliar_str(void) {
         exit(-1);
     }
     return auxiliar;
+}
+
+void initialize_pointers(struct auxiliar_words *auxiliar, char *word) {
+    int size_ptr = 0, place = 0;
+    size_ptr = strlen(word);
+    place = which_way(auxiliar);
+    switch (place) {
+        case 1:{
+            auxiliar->first_word = (char *)calloc(size_ptr+1,sizeof(char));
+            strcpy(auxiliar->first_word,word);
+            break;
+        }
+        case 2:{
+            auxiliar->next_word = (char *)calloc(size_ptr+1,sizeof(char));
+            strcpy(auxiliar->next_word,word);
+            // Additional push
+            break;
+        }
+    }
 }
