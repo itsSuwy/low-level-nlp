d53220421f026123dd63666428a608e79c8f9124
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-31 02:09:13 -0600

    feat: Declare text extraction prototypes and dependencies

--- include/01_Reading.h
+++ include/01_Reading.h
@@ -5,4 +5,18 @@
 #ifndef LOW_LEVEL_NLP_01_READING_H
 #define LOW_LEVEL_NLP_01_READING_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../include/02_Node-STR.h"
+
+void reading_file(struct graph *graph);
+char *word_package(void);
+void extraction(char *original_string, char *word, char character, struct graph *graph);
+char pop(char *original_string);
+char *merge(char *word, char character);
+int validation(char *original_string, char *word);
+
+
+
 #endif //LOW_LEVEL_NLP_01_READING_H
