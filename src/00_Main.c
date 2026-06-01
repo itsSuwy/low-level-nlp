ee49380c8e125e0e262a8457e0987f8dcf719d9b
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-31 22:38:25 -0600

    feat: integrate graph pipeline and implement recursive debug printing

--- src/00_Main.c
+++ src/00_Main.c
@@ -1,11 +1,40 @@
 //
 // Created by suwy on 5/25/26.
 //
+
 #include <stdio.h>
-#include <stdlib.h>
+#include "../include/01_Reading.h"
+#include "../include/02_Node-STR.h"
+
+void impresion(struct node *node);
+void impresion_edges(struct edge *edge);
 
 int main(int argc, char *argv[]) {
-    puts("HELLO CYKA BLYAAAAAAAAAAAAAAAAAAAAAAAAAAT");
+    struct graph *graph = initialise_graph();
+    reading_file(graph);
+    impresion(graph->start);
 
     return 0;
+}
+
+void impresion(struct node *node) {
+    if (!node) {
+        return;
+    }
+    printf("Palabra asignada: %s\n", node->word);
+    printf("Apariciones: %i\n", node->occurrences);
+    printf("Apunta a:\n");
+    impresion_edges(node->first_connection);
+    printf("\n");
+    impresion(node->next_node);
+}
+
+void impresion_edges(struct edge *edge) {
+    if (!edge) {
+        return;
+    }
+    printf("\t%s\n", edge->node->word);
+    printf("\tSe repite %i veces\n", edge->occurrences);
+    printf("\n");
+    impresion_edges(edge->next_edge);
 }
