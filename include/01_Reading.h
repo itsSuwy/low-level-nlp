886ef04340e0e146e67276c7d89ae7021667f97e
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-31 22:31:32 -0600

    refactor: update header file declarations

--- include/01_Reading.h
+++ include/01_Reading.h
@@ -5,18 +5,17 @@
 #ifndef LOW_LEVEL_NLP_01_READING_H
 #define LOW_LEVEL_NLP_01_READING_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "../include/02_Node-STR.h"
 
 void reading_file(struct graph *graph);
 char *word_package(void);
-void extraction(char *original_string, char *word, char character, struct graph *graph);
+char *special_word(void);
+void extraction(char *original_string, char *word, struct graph *graph);
+int validation_V2(char *original_string, char *word);
 char pop(char *original_string);
 char *merge(char *word, char character);
-int validation(char *original_string, char *word);
-
-
+char *spanish_merge(char *word, char *spanish_character);
+unsigned char spanish_mayus_to_minus(unsigned char letter);
+char *fill_special_word(char *original_string, char *spanish_word);
 
 #endif //LOW_LEVEL_NLP_01_READING_H
