d029f368f3466639a03feb09fcc86cd12cffc389
itsSuwy <diego.velazquez.2006@outlook.es>
2026-06-02 01:07:59 -0600

    refactor: clean up public header API to expose only main reading function

--- include/01_Reading.h
+++ include/01_Reading.h
@@ -8,14 +8,5 @@
 #include "../include/02_Node-STR.h"
 
 void reading_file(struct graph *graph);
-char *word_package(void);
-char *special_word(void);
-void extraction(char *original_string, char *word, struct graph *graph);
-int validation_V2(char *original_string, char *word);
-char pop(char *original_string);
-char *merge(char *word, char character);
-char *spanish_merge(char *word, char *spanish_character);
-unsigned char spanish_mayus_to_minus(unsigned char letter);
-char *fill_special_word(char *original_string, char *spanish_word);
 
 #endif //LOW_LEVEL_NLP_01_READING_H
