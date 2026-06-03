f027b56bdad6104891f587bc776cef75af14eef6
itsSuwy <diego.velazquez.2006@outlook.es>
2026-06-03 00:45:04 -0600

    refactor: clean up main entry point to simply launch the UI menu

--- src/00_Main.c
+++ src/00_Main.c
@@ -2,14 +2,9 @@
 // Created by suwy on 5/25/26.
 //
 
-#include <stdio.h>
-#include "../include/01_Reading.h"
-#include "../include/02_Node-STR.h"
+#include "../include/05_Utils.h"
 
 int main(int argc, char *argv[]) {
-    struct graph *graph = initialise_graph();
-    reading_file(graph);
-    graph_probability(graph);
-    print_graph(graph);
+    menu();
     return 0;
 }
