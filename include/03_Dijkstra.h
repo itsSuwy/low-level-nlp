2e09192833570667a8d1f179795fa1dae1ebe69f
itsSuwy <diego.velazquez.2006@outlook.es>
2026-06-16 14:52:56 -0600

    feat: add header file with Dijkstra structure and public function with the same name

--- include/03_Dijkstra.h
+++ include/03_Dijkstra.h
@@ -5,4 +5,16 @@
 #ifndef LOW_LEVEL_NLP_03_DIJKSTRA_H
 #define LOW_LEVEL_NLP_03_DIJKSTRA_H
 
+#include <stdbool.h>
+
+struct dijkstra {
+    struct dijkstra *next;
+    struct node *current_node;
+    struct node *previous_node;
+    float probability;
+    bool visited;
+};
+
+void Dijkstra(struct graph *graph, char *word);
+
 #endif //LOW_LEVEL_NLP_03_DIJKSTRA_H
